49ff7b24a213975fec978717e60bf028254d1a80
Pollyany Carvalho <kerleypollyany@gmail.com>
2023-10-11 10:34:55 -0300

    Criando Context Api.

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -7,6 +7,14 @@ export default function SignUp() {
   const [password, setPassword] = useState('')
   const [name, setName] = useState('')
 
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    if(name !== '' && email !== '' && password !== '') {
+      alert('Fazer cadastro')
+    }
+  }
+
   return(
     <div className='container-center'>
       <div className="login">
@@ -14,7 +22,7 @@ export default function SignUp() {
           <img src={logo} alt="Logo do sistema de chamados" />
         </div>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Nova conta</h1>
           <input 
           type="text"
